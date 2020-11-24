284cf008e1d4973dc7034ebb7d7cba8fb7ce4497
Tina Armstead <tina.armstead@gmail.com>
2020-11-23 21:52:26 -0500

    Added returns and improved indentation/spacing

--- script.js
+++ script.js
@@ -1,60 +1,78 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    let password = generatePassword();
+    let passwordText = document.querySelector("#password");
+    passwordText.value = password;
+
+    //   document.querySelector("textarea").value = generatePassword();
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-var lettersCapitalToChoose = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J","K", "L", "M", "N", "O", "P", "Q", "R", "S", "T","U", "V", "W", "X", "Y", "Z",];
 
-var lettersLowerCaseToChoose = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j","k", "l", "m", "n", "o", "p", "q", "r", "s", "t","u", "v", "w", "x", "y", "z",]; 
 
-var numbersToChoose = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-var specialCharToChoose = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",];
+function generatePassword() {
+    let lengthOfPassword = parseInt(prompt("How many characters do you want to include in your password"));
+    
+    if (lengthOfPassword < 8 || lengthOfPassword > 128) {
+        return alert("Please select a number greater than 7 and less than 129.");
+    }
+    
+    let lettersCapitalToChoose = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
+    let lettersLowerCaseToChoose = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+    let numbersToChoose = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let specialCharToChoose = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")",];
 
-var charPot = [];
+    let lowerCase = confirm("Do you want to include lower case letters?");
+    let upperCase = confirm("Do you want to include upper case letters?");
+    let numbers = confirm("Do you want to include numbers?");
+    let specialChar = confirm("Do you want to include special characters?");
 
-function generatePassword() { 
-    var lengthOfPassword = prompt("How many characters do you want to include in your password");
-        if (lengthOfPassword < 8 || lengthOfPassword > 128) {
-        alert("Please select a number greater than 7 and less than 129.");
-        prompt("How many characters do you want to include in your password");
-}
 
-    var lowerCase = confirm("Do you want to include lower case letters?");
-        if (lowerCase) {
+
+    if (
+        !lowerCase &&
+        !upperCase &&
+        !numbers &&
+        !specialChar
+    ) {
+        return alert("Please select atleast one character type.");    
+    }
+
+    let charPot = [];
+
+    if (lowerCase) {
         charPot = charPot.concat(lettersLowerCaseToChoose);
-}
+    }
 
-    var upperCase = confirm("Do you want to include upper case letters?");
-        if (upperCase) {
+    if (upperCase) {
         charPot = charPot.concat(lettersCapitalToChoose);
-} 
+    }
 
-    var numbers = confirm("Do you want to include numbers?");
-        if (numbers) {
+    if (numbers) {
         charPot = charPot.concat(numbersToChoose);
-} 
+    }
 
-    var specialChar = confirm("Do you want to include special characters?");
-        if (specialChar) {
+    if (specialChar) {
         charPot = charPot.concat(specialCharToChoose);
-}
-    var final = "";
-    for(var i = 0; i < lengthOfPassword; i++) {
-        var potPull = Math.floor(Math.random() * charPot.length);
-        final += charPot[potPull];
-        console.log(final);
-}
-    return final;
+    }
+    let final = [];
+    for (let i = 0; i < lengthOfPassword; i++) {
+        let potPull = Math.floor(Math.random() * charPot.length-1);
+        let temp = charPot[potPull];
+        final.push(temp);
+        // final += charPot[potPull];
+        // console.log(final);
+    }
+    return final.join("");
 }
 
-generatePassword();
+
 
 
